2de6c273193dbef237c58e290f20ac994fdc08cc
Anton Galkin <artemis9898@mail.ru>
2018-06-25 12:16:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,33 +10,37 @@
   $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
   ));
+  
   $app->get('/', function () use($app) {
     return $app['twig']->render('home.twig', array(
-      'content' => 'Welcome!'
+      'content' => 'Добро пожаловать!'
     ));  
   });
+  
   $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Сумма равна: </h2><h3>' . $n1 . '+' . $n2 . '=' . ($n1 + $n2) . '</h3>'; 
+    return '<h2>Сумма: </h2><h3>' . ($n1 + $n2) . '</h3>'; 
   })->after($hdrs);
-
-  $app->get('/mpy/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Произведение равно: </h2><h3>' . $n1 . '*' . $n2 . '=' . ($n1 * $n2) . '</h3>'; 
-  })->after($hdrs);
-
   $app->get('/sub/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Разность равна: </h2><h3>' . $n1 . '-' . $n2 . '=' . ($n1 - $n2) . '</h3>'; 
+    return '<h2>Вычитание: </h2><h3>' . ($n1 - $n2) . '</h3>'; 
   })->after($hdrs);
-  
   $app->get('/div/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Частное равно: </h2><h3>' . $n1 . '/' . $n2 . '=' . ($n1 / $n2) . '</h3>'; 
+    return '<h2>Деление: </h2><h3>' . ($n1 / $n2) . '</h3>'; 
   })->after($hdrs);
-
+  $app->get('/mul/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Умножение: </h2><h3>' . ($n1 * $n2) . '</h3>'; 
+  })->after($hdrs);
   $app->get('/pow/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Степень равна: </h2><h3>' . $n1 . '^' . $n2 . '=' . (pow($n1, $n2)) . '</h3>'; 
+    return '<h2>Возведение в степень: </h2><h3>' . ($n1 ** $n2) . '</h3>'; 
   })->after($hdrs);
   
-  $app->get('/author', function () use($app) {
-    return '<h4 id="author" title="GossJS">Galkin Anton</h4>';
+   $app->get('/author', function () use($app) {
+    return '<h4 id="author" title="GossJS">Мария Шибаева</h4>'; 
   })->after($hdrs);
-
+  $app->error(function ($e) use($app) {
+    if ($e instanceof Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+      return new Response($app['twig']->render('404.twig'), 404);
+    } else {
+      return new Response($app['twig']->render('500.twig'), 500);
+    };
+  });
   $app->run();
