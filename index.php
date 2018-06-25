545f9a2448b07e9d76d9f60ebad92f68056e1218
Anton Galkin <artemis9898@mail.ru>
2018-06-25 11:52:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -18,12 +18,11 @@
   $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
     return '<h2>Сумма равна: </h2><h3>' . $n1 . '+' . $n2 . '=' . ($n1 + $n2) . '</h3>'; 
   })->after($hdrs);
-$app->get('/111/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Сумма равна: </h2><h3>' . $n1 . '+' . $n2 . '=' . ($n1 + $n2) . '</h3>'; 
-  })->after($hdrs);
+
   $app->get('/mpy/{n1}/{n2}', function ($n1, $n2) use($app) {
     return '<h2>Произведение равно: </h2><h3>' . $n1 . '*' . $n2 . '=' . ($n1 * $n2) . '</h3>'; 
   })->after($hdrs);
+
   $app->get('/sub/{n1}/{n2}', function ($n1, $n2) use($app) {
     return '<h2>Разность равна: </h2><h3>' . $n1 . '-' . $n2 . '=' . ($n1 - $n2) . '</h3>'; 
   })->after($hdrs);
@@ -31,6 +30,7 @@ $app->get('/111/{n1}/{n2}', function ($n1, $n2) use($app) {
   $app->get('/div/{n1}/{n2}', function ($n1, $n2) use($app) {
     return '<h2>Частное равно: </h2><h3>' . $n1 . '/' . $n2 . '=' . ($n1 / $n2) . '</h3>'; 
   })->after($hdrs);
+
   $app->get('/pow/{n1}/{n2}', function ($n1, $n2) use($app) {
     return '<h2>Степень равна: </h2><h3>' . $n1 . '^' . $n2 . '=' . (pow($n1, $n2)) . '</h3>'; 
   })->after($hdrs);
@@ -38,11 +38,5 @@ $app->get('/111/{n1}/{n2}', function ($n1, $n2) use($app) {
   $app->get('/author', function () use($app) {
     return '<h4 id="author" title="GossJS">Galkin Anton</h4>';
   })->after($hdrs);
-  $app->error(function ($e) use($app) {
-    if ($e instanceof Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
-      return new Response($app['twig']->render('404.twig'), 404);
-    } else {
-      return new Response($app['twig']->render('500.twig'), 500);
-    };
-  });
+
   $app->run();
